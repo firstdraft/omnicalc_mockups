a4fd295c5a6d1dc754ed9da7cd55018e7e928869
Raghu Betina <raghu@firstdraft.com>
2016-03-10 13:30:48 -0600

    Add percy and rest of specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,4 +89,11 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.before(:suite) do
+    Percy.config.access_token = ENV["PERCY_TOKEN"]
+  end
+
+  config.before(:suite) { Percy::Capybara.initialize_build }
+  config.after(:suite) { Percy::Capybara.finalize_build }
 end
--- spec/support/percy.rb
+++
@@ -1,8 +0,0 @@
-RSpec.configure do |config|
-  config.before(:suite) do
-    Percy.config.access_token = ENV["PERCY_TOKEN"]
-  end
-
-  config.before(:suite) { Percy::Capybara.initialize_build }
-  config.after(:suite) { Percy::Capybara.finalize_build }
-end
